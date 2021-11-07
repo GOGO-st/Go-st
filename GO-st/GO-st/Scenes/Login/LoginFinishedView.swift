e649a36a84b0f34dc1b317c2b4097cbc24cc035d
yujin <glisteneugene@gmail.com>
2021-11-07 15:33:53 +0900

    로그인 뷰 3개 생성

---
+++ GO-st/GO-st/Scenes/Login/LoginFinishedView.swift
@@ -0,0 +1,31 @@
+//
+//  LoginFinishedView.swift
+//  GO-st
+//
+//  Created by ✨EUGENE✨ on 2021/11/07.
+//
+
+import UIKit
+import Then
+import SnapKit
+
+final class LoginFinishedView: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.addContentView()
+        self.setAutoLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    private func addContentView() {
+        
+    }
+    
+    private func setAutoLayout() {
+        
+    }
+}
---
+++ GO-st/GO-st/Scenes/Login/LoginNumberView.swift
@@ -0,0 +1,31 @@
+//
+//  LoginNumberView.swift
+//  GO-st
+//
+//  Created by ✨EUGENE✨ on 2021/11/07.
+//
+
+import UIKit
+import Then
+import SnapKit
+
+final class LoginNumberView: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.addContentView()
+        self.setAutoLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    private func addContentView() {
+        
+    }
+    
+    private func setAutoLayout() {
+        
+    }
+}
