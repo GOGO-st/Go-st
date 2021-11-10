cbe4e308fb378e05f29f08015fce396b409d5189
yujin <glisteneugene@gmail.com>
2021-11-10 14:46:41 +0900

    signupviewmodel

---
+++ GO-st/GO-st/Scenes/SignUp/Step1/SignUpViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  SignUpViewModel.swift
+//  GO-st
+//
+//  Created by ✨EUGENE✨ on 2021/11/10.
+//
+
+import Foundation
+
+final class SignUpViewModel {
+    
+    // 이메일 정규성 체크
+    func validateEmail(_ email: String) -> Bool {
+        let regex = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+        let emailPredicate = NSPredicate(format: "SELF MATCHES %@", regex)
+        return emailPredicate.evaluate(with: email)
+    }
+}
