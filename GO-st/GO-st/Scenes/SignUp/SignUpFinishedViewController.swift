d9908050f87b8facfe791a3a8990579fc8f662ca
yujin <glisteneugene@gmail.com>
2021-11-19 14:32:11 +0900

    회원가입 완료 뷰 NSMutableAttribute

--- GO-st/GO-st/Scenes/SignUp/SignUpFinishedViewController.swift
+++ GO-st/GO-st/Scenes/SignUp/SignUpFinishedViewController.swift
@@ -13,10 +13,17 @@ final class SignUpFinishedViewController: UIViewController {
     
     static let identifier = "SignUpFinishedViewController"
     
-    private let descriptionLabel = UILabel().then {
+    private let finishedLabel = UILabel().then {
         $0.text = "회원가입 완료!"
         $0.textColor = .white
-        $0.font = .boldSystemFont(ofSize: 20)
+        $0.font = .boldSystemFont(ofSize: 24)
+    }
+    
+    private let descriptionLabel = UILabel().then {
+        $0.text = ""
+        $0.textColor = .white
+        $0.textAlignment = .center
+        $0.numberOfLines = 3
     }
     
     private let nextButton = FinishedButton(title: "만나러 가기").then {
@@ -32,9 +39,12 @@ final class SignUpFinishedViewController: UIViewController {
         self.setAutoLayout()
         
         self.nextButton.addTarget(self, action: #selector(nextButtonDidTap), for: .touchUpInside)
+        
+        self.setNickName("상큼한 혼밥러")
     }
     
     private func addContentView() {
+        view.addSubview(finishedLabel)
         view.addSubview(descriptionLabel)
         view.addSubview(nextButton)
     }
@@ -43,11 +53,24 @@ final class SignUpFinishedViewController: UIViewController {
         
         let safeArea = view.safeAreaLayoutGuide
         
-        descriptionLabel.snp.makeConstraints {
+        finishedLabel.snp.makeConstraints {
             $0.center.equalTo(safeArea)
+        }
+        
+        descriptionLabel.snp.makeConstraints {
+            $0.centerX.equalTo(safeArea)
+            $0.top.equalTo(finishedLabel.snp.bottom).offset(22)
         }
     }
     
+    private func setNickName(_ nickName: String) {
+        let attributString = NSMutableAttributedString()
+            .bold("\(nickName)님", fontSize: 14)
+            .normal("\n학교 유령들이 남긴 흔적들을", fontSize: 14)
+            .normal("\n만날 준비 되셨나요?", fontSize: 14)
+        
+        self.descriptionLabel.attributedText = attributString
+    }
     @objc
     private func nextButtonDidTap() {
         self.navigationController?.pushViewController(TabBarViewController(), animated: false)
