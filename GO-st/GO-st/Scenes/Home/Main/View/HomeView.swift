a01b65617894286c6752a854e5a9a65b9a3f933d
yujin <glisteneugene@gmail.com>
2021-11-25 22:17:18 +0900

    goView

--- GO-st/GO-st/Scenes/Home/Main/View/HomeView.swift
+++ GO-st/GO-st/Scenes/Home/Main/View/HomeView.swift
@@ -18,12 +18,15 @@ final class HomeView: UIView {
     let mapView = MKMapView()
     
     // 가보자고
-    private let goView = UIView().then {
-        $0.backgroundColor = .systemIndigo
+    private lazy var goView = UIView().then {
+        $0.backgroundColor = R.color.semiBlack()
+        $0.layer.cornerRadius = 8 * goViewHeight / 64
     }
     
     private let goLabel = UILabel().then {
-        $0.text = "어디 한 번 가보자고"
+        $0.text = "흔적 찾아 떠나보자고!"
+        $0.font = R.font.notoSansKRRegular(size: 20)
+        $0.textColor = .white
     }
     
     let goButton = UIButton().then {
@@ -42,6 +45,9 @@ final class HomeView: UIView {
     let storeInfoView = StoreInfoCardView().then {
         $0.isHidden = true
     }
+    
+    private let goViewWidth = CommonValue.shared.WIDTH - 40
+    private lazy var goViewHeight = goViewWidth * 64 / 335
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -76,21 +82,22 @@ final class HomeView: UIView {
     }
     
     private func setAutoLayout() {
-        let safeArea = self.safeAreaLayoutGuide
+//        let safeArea = self.safeAreaLayoutGuide
         
         // 지도
         mapView.snp.makeConstraints {
-            $0.top.equalToSuperview()
-            $0.left.right.bottom.equalTo(safeArea)
+//            $0.top.equalToSuperview()
+            $0.top.left.right.bottom.equalTo(self)
         }
         
         // 가보자고
         goView.snp.makeConstraints {
-            $0.top.equalTo(safeArea).offset(50)
-            $0.left.equalTo(safeArea).offset(20)
-            $0.right.equalTo(safeArea).offset(-20)
-            $0.height.equalTo(50)
+            $0.top.equalTo(self).offset(50)
+            $0.centerX.equalTo(self)
+            $0.width.equalTo(goViewWidth)
+            $0.height.equalTo(goViewHeight)
         }
+        
         goLabel.snp.makeConstraints {
             $0.center.equalTo(goView)
         }
@@ -101,15 +108,15 @@ final class HomeView: UIView {
         // 가게 재검색
         retrieveButton.snp.makeConstraints {
             $0.top.equalTo(goView.snp.bottom).offset(10)
-            $0.centerX.equalTo(safeArea)
+            $0.centerX.equalTo(self)
             $0.width.equalTo(100)
         }
         
         // 가게 정보 뷰
         storeInfoView.snp.makeConstraints {
-            $0.bottom.equalTo(safeArea).offset(-10)
-            $0.left.equalTo(safeArea).offset(15)
-            $0.right.equalTo(safeArea).offset(-14)
+            $0.bottom.equalTo(self).offset(-10)
+            $0.left.equalTo(self).offset(15)
+            $0.right.equalTo(self).offset(-14)
         }
         
     }
@@ -120,7 +127,8 @@ final class HomeView: UIView {
         retrieveButton.isHidden.toggle()
     }
     
-    @objc func retrieveButtonDidTap(_ sender: UIButton) {
-        retrieveButton.isHidden = true
+    @objc
+    func retrieveButtonDidTap(_ sender: UIButton) {
+        retrieveButtonIsHidden()
     }
 }
